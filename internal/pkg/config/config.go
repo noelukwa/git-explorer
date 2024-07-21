c18ed024993ae44b184dbdcf390d046987d47969
noelukwa <ukwauchechukwu@gmail.com>
2024-07-21 06:32:02 +0100

    feat: setup worker

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -1,9 +1,22 @@
 package config
 
+import "time"
+
 type ExplorerConfig struct {
 	DatabaseURL       string `split_words:"true" default:"postgres://explorer:explorer2025@localhost/explorer?sslmode=disable"`
 	Port              int    `default:"9800"`
 	TestDatabaseURL   string `split_words:"true" required:"true"`
 	MessagingProvider string `split_words:"true" default:"nats"`
 	MessagingURL      string `split_words:"true" required:"true"`
+}
+
+type ExplorerdConfig struct {
+	GithubToken        string        `split_words:"true" required:"true"`
+	MessagingProvider  string        `split_words:"true" default:"nats"`
+	MessagingURL       string        `split_words:"true" required:"true"`
+	BatchSize          int           `split_words:"true" default:"10"`
+	MaxRetries         int           `envconfig:"MAX_RETRIES" default:"3"`
+	BackoffInitial     time.Duration `split_words:"true" default:"1s"`
+	BackoffMax         time.Duration `split_words:"true" default:"1m"`
+	MonitoringInterval time.Duration `split_words:"true" default:"1m"`
 }
